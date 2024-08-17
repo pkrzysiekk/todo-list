98b38e53b17ab72c9f051c5d69d5ebbe433d5629
pkrzysiekk <pkrzysiek24@gmail.com>
2024-08-17 19:37:10 +0200

    Add modal autoclose functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { getNewProject } from "./getNewProject";
-
+import { modalAutoClose } from "./modalAutoCLose";
 const addProjectButton=document.querySelector("#add");
+const modal=document.querySelector("#modal");
 
 addProjectButton.addEventListener("click",getNewProject);
+modal.addEventListener("click",modalAutoClose);
---
+++ src/modalAutoCLose.js
@@ -0,0 +1,13 @@
+
+export function modalAutoClose(e){
+    const dialogDimensions = modal.getBoundingClientRect()
+   
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      modal.close()
+    }
+}
