2f791cbab4d4606451f901a0f225de40c36aec2c
pkrzysiekk <pkrzysiek24@gmail.com>
2024-08-18 17:40:14 +0200

    Add displaying project title in the main section

--- src/openProject.js
+++ src/openProject.js
@@ -1,8 +1,15 @@
 import { projectList } from "./projectList";
-
+import { clearElement } from "./clearElement";
 export function openProject(e){
     const button=e.target;
     const projectName=button.textContent;
+    const mainDiv=document.querySelector("#main")
     let index=_.findIndex(projectList,function (project){ return project.title===projectName});
-    console.log(index);
+    const titleDiv=document.querySelector(".title");
+    const titleSpan=document.createElement("span");
+    clearElement(titleDiv);
+    let projectToDisplay=projectList[index];
+    titleSpan.textContent=projectToDisplay.title;
+    titleDiv.appendChild(titleSpan);
+
 }
